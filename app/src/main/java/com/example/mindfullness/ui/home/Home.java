304627bd74a3f6ec9fd037e23cdac9c703937be8
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-03-31 23:26:12 +0200

    Add displaying articles on homepage of app

--- app/src/main/java/com/example/mindfullness/ui/home/Home.java
+++ app/src/main/java/com/example/mindfullness/ui/home/Home.java
@@ -1,7 +1,6 @@
 package com.example.mindfullness.ui.home;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,50 +8,70 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.mindfullness.databinding.FragmentHomeBinding;
 import com.example.mindfullness.helpers.HTTPJson;
+import com.example.mindfullness.types.Article;
+import com.example.mindfullness.adapters.ArticleAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class Home extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
         textView.setText("This is home fragment");
+
+        // Dodanie menedżera układu do RecyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        binding.articlesView.setLayoutManager(layoutManager);
+
         displayArticles();
+
         return root;
     }
 
     public void displayArticles() {
-        Log.d("Article", "Displaying articles");
         HTTPJson api = new HTTPJson();
         api.sendGetRequest("https://jsonplaceholder.typicode.com/posts/", new HTTPJson.OnResponseListener() {
             @Override
             public void onResponse(String response) {
                 try {
                     JSONArray articlesArray = new JSONArray(response);
+                    ArrayList<Article> articles = new ArrayList<>();
                     for (int i = 0; i < articlesArray.length(); i++) {
                         JSONObject article = articlesArray.getJSONObject(i);
-                        Log.d("Article " + i, article.toString());
+                        Article articleCasted = new Article(article.getString("title"), article.getString("body"));
+                        articles.add(articleCasted);
                     }
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArticleAdapter adapter = new ArticleAdapter(articles);
+                            binding.articlesView.setAdapter(adapter);
+                        }
+                    });
+
                 } catch (JSONException e) {
                     e.printStackTrace();
-                    Log.e("Error", "Error parsing JSON response: " + e.getMessage());
                 }
             }
 
             @Override
             public void onError(String error) {
-                Log.e("Error", "Error while fetching articles: " + error);
+                // Obsługa błędu
             }
         });
     }
