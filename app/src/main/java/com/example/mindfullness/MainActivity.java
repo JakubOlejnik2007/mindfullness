49e92b7c4cf5b964821f3691596ac4f536aa303e
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-03-31 21:25:32 +0200

    Hide titles of unactive navigation items

--- app/src/main/java/com/example/mindfullness/MainActivity.java
+++ app/src/main/java/com/example/mindfullness/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.mindfullness;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.mindfullness.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        for (int i = 1; i < navView.getMenu().size(); i++) {
+            navView.getMenu().getItem(i).setTitle(null);
+        }
+
+        navView.setOnNavigationItemSelectedListener(item -> {
+            for (int i = 0; i < navView.getMenu().size(); i++) {
+                int itemId = navView.getMenu().getItem(i).getItemId();
+                String title = getResources().getString(getTitleResById(itemId));
+                navView.getMenu().getItem(i).setTitle(title);
+            }
+
+            for (int i = 0; i < navView.getMenu().size(); i++) {
+                navView.getMenu().getItem(i).setTitle(null);
+            }
+
+            item.setTitle(getResources().getString(getTitleResById(item.getItemId())));
+
+            return NavigationUI.onNavDestinationSelected(item, navController);
+        });
+    }
+
+    private int getTitleResById(int itemId) {
+        String[] parts = getResources().getResourceEntryName(itemId).split("_");
+        String resourceName = "title_" + parts[1];
+        return getResources().getIdentifier(resourceName, "string", getPackageName());
     }
 
 }
