007236b414f4c63bc4eb43e390c58c07a3151485
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-03-31 22:58:49 +0200

    Add displaying articles from api

---
+++ app/src/main/java/com/example/mindfullness/adapters/ArticleAdapter.java
@@ -0,0 +1,49 @@
+package com.example.mindfullness.adapters;
+
+import com.example.mindfullness.types.Article;
+import com.example.mindfullness.R;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+    private List<Article> dataSet;
+
+    public ArticleAdapter(List<Article> dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.article_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TextView textViewTitle = holder.itemView.findViewById(R.id.textViewTitle);
+        textViewTitle.setText(dataSet.get(position).title);
+        TextView textViewBody = holder.itemView.findViewById(R.id.textViewBody);
+        textViewBody.setText(dataSet.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
---
+++ app/src/main/java/com/example/mindfullness/types/Article.java
@@ -0,0 +1,13 @@
+package com.example.mindfullness.types;
+
+public class Article {
+    public String title;
+    public String description;
+    public String content;
+    public String image;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
