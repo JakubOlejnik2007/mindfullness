151bd29954435616a3ec763d3867dab4fc2935cf
Mati <m.stoch2007@gmail.com>
2024-04-02 00:52:12 +0200

    made the colors actually work on other devices

--- app/src/main/java/com/example/mindfullness/MainActivity.java
+++ app/src/main/java/com/example/mindfullness/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             item.setTitle(getResources().getString(getTitleResById(item.getItemId())));
+
             return NavigationUI.onNavDestinationSelected(item, navController);
         });
     }
