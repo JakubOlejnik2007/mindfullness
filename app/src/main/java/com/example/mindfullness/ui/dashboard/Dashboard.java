3cbe3d22f0dacf50063d163738321798e43ebadf
Krystian Cichocki <lewer183@wp.pl>
2024-04-06 21:56:37 +0200

    I added loading data from shared preferences

--- app/src/main/java/com/example/mindfullness/ui/dashboard/Dashboard.java
+++ app/src/main/java/com/example/mindfullness/ui/dashboard/Dashboard.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.mindfullness.databinding.FragmentDashboardBinding;
+import com.example.mindfullness.helpers.SharedPreferencesManager;
+
 
 public class Dashboard extends Fragment {
 
@@ -20,11 +22,34 @@ public class Dashboard extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         final TextView textView = binding.textDashboard;
         textView.setText("Konto");
 
+
+
+
+        TextView nameTextView = binding.name;
+        TextView emailTextView = binding.email;
+
+
+        String[] userData = SharedPreferencesManager.readData(getContext());
+        String name = userData[1];
+        String email = userData[2];
+
+
+        if (name.isEmpty() || email.isEmpty()) {
+            nameTextView.setText("Brak danych");
+            emailTextView.setText("Brak danych");
+        } else {
+
+            nameTextView.setText(name);
+            emailTextView.setText(email);
+        }
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
